4a26aa6bd3d9e6d49b09bb9953cedf83e79ac26c
mingmeng030 <djwls_0716@naver.com>
2022-07-30 18:30:46 +0900

    add testResult page

--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -1,20 +1,77 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./test.css";
+import "./testResult.css";
 import tastes from "./tastes";
 import questions from "./questions";
 import Radio from "./Radio";
+import { useNavigate } from "react-router-dom";
+
+interface Review {
+  star: number;
+  id: number;
+  image: string;
+}
+interface visitProps {
+  a_id: number;
+  female: number;
+  fiftys: number;
+  fourtys: number;
+  male: number;
+  thirtys: number;
+  twentys: number;
+}
+interface BoardDetailState {
+  capacity: number;
+  content: string;
+  degree: number;
+  id: number;
+  image: string;
+  manufacturer: string;
+  name: string;
+  price: number;
+  reviews: Array<Review>;
+  taste_1: string;
+  taste_2: string;
+  taste_3: string;
+  taste_4: string;
+  taste_5: string;
+  type: string;
+  zzim: boolean;
+  visit: visitProps;
+}
+interface resultProps {
+  capacity: number;
+  content: string;
+  degree: number;
+  id: number;
+  image: string;
+  manufacturer: string;
+  name: string;
+  price: number;
+  taste_1: string;
+  taste_2: string;
+  taste_3: string;
+  taste_4: string;
+  taste_5: string;
+  type: string;
+  visit: visitProps;
+}
+
 const Test = () => {
+  const navigate = useNavigate();
   const background = require("../static/banner3.png");
 
   const [selectedMenu, setSelectedMenu] = useState("page1");
   const [seletedTypeIdx, setSelectedTypeIdx] = useState<number>(0);
-  const [degree, setDegree] = useState<string>("");
+  const [degree, setDegree] = useState<string>("25도 이하");
   const [taste1, setTaste1] = useState<string>("없음");
   const [taste2, setTaste2] = useState<string>("없음");
   const [taste3, setTaste3] = useState<string>("없음");
   const [taste4, setTaste4] = useState<string>("없음");
   const [taste5, setTaste5] = useState<string>("없음");
+
+  const [result, setResult] = useState<Array<resultProps>>([]);
 
   const onNextClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -61,10 +118,19 @@ const Test = () => {
         parentNode[8].classList.remove("hide");
         setSelectedMenu((selectedMenu) => (selectedMenu = "page8"));
         showResult();
-      } else if (className == "next-button8 next-button") {
+      } else if (className == "next-button8 return-button") {
         // 처음으로 버튼
+
         parentNode[0].classList.remove("hide");
         setSelectedMenu((selectedMenu) => (selectedMenu = "page1"));
+        setTaste1("없음");
+        setTaste2("없음");
+        setTaste3("없음");
+        setTaste4("없음");
+        setTaste5("없음");
+        setSelectedTypeIdx(0);
+        setResult([]);
+        setDegree("25도 이하");
       }
     }
   };
@@ -79,9 +145,8 @@ const Test = () => {
       taste4: taste4,
       taste5: taste5,
     };
-    console.log(result);
     axios({
-      method: "GET",
+      method: "POST",
       url: `/recommendation`,
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -90,26 +155,70 @@ const Test = () => {
       data: result,
     })
       .then((res) => {
-        console.log(res);
+        setResult(res.data.slice(0, 3));
       })
       .catch((err) => {
         console.log(" 결과 가져오기 에러", err);
       });
   };
+  const onImageClick = (id: number) => {
+    return (event: React.MouseEvent) => {
+      if (localStorage.getItem("token")) {
+        axios({
+          method: "GET",
+          url: `/board/${id}`,
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        })
+          .then((res) => {
+            console.log(res.data);
+
+            const s: BoardDetailState = {
+              capacity: res.data.alcoholDetail.capacity,
+              content: res.data.alcoholDetail.content,
+              degree: res.data.alcoholDetail.degree,
+              id: res.data.alcoholDetail.id,
+              image: res.data.alcoholDetail.image,
+              manufacturer: res.data.alcoholDetail.manufacturer,
+              name: res.data.alcoholDetail.name,
+              price: res.data.alcoholDetail.price,
+              reviews: res.data.alcoholDetail.reviews,
+              taste_1: res.data.alcoholDetail.taste_1,
+              taste_2: res.data.alcoholDetail.taste_2,
+              taste_3: res.data.alcoholDetail.taste_3,
+              taste_4: res.data.alcoholDetail.taste_4,
+              taste_5: res.data.alcoholDetail.taste_5,
+              type: res.data.alcoholDetail.type,
+              zzim: res.data.zzim,
+              visit: res.data.visit,
+            };
+            navigate(`/board/${id}`, {
+              state: { boardDetail: s },
+            });
+          })
+          .catch((err) => {
+            console.log("상세 페이지 가져오기 에러", err);
+          });
+      } else {
+        alert("로그인 후 이용할 수 있습니다.");
+        navigate(`/login`);
+      }
+    };
+  };
 
   return (
     <div className="Test-Top-Container">
       <img className="background" src={background} />
-      <div className="Test-header-container">
+      {/* <div className="Test-header-container">
         <p className="Test-header">나만의 전통주 알아보기</p>
-      </div>
+      </div> */}
       <div className="Test-bottom-container">
         <div className="Test-page page1">
-          <p className="Test-content">
-            시작하기를 눌러 나만의 전통주를 알아보세요!
-          </p>
+          <p className="Test-content">당신의 취향에 맞는</p>
+          <p className="Test-content">전통주를 알아보세요!</p>
           <button className="next-button1 start-button" onClick={onNextClick}>
-            시작하기
+            테스트 시작
           </button>
         </div>
         {/* 주종 선택 */}
@@ -159,7 +268,7 @@ const Test = () => {
             다음
           </button>
         </div>
-
+        {/* 증류주-도수 선택 */}
         <div className="Test-page page25 hide">
           <p className="question">증류주의 도수를 골라주세요</p>
           <p className="answer">
@@ -186,7 +295,6 @@ const Test = () => {
             다음
           </button>
         </div>
-
         {/* 맛1 선택 */}
         <div className="Test-page page3 hide">
           <Radio
@@ -248,32 +356,29 @@ const Test = () => {
           </button>
         </div>
         <div className="Test-page page8 hide">
-          <div>
-            <p>결과</p>
-            <p>주종 : {tastes[seletedTypeIdx].type}</p>
-            <p>도수 : {degree}</p>
+          <p className="Test-result-Header">당신의 취향 분석 결과 입니다</p>
+          <div className="Test-result-Container-Outer">
+            {result.length === 3 &&
+              result.map((value, i: number) => (
+                <div className="Test-result-Container-Inner">
+                  <div className="test-img-container">
+                    <img
+                      className="Test-result-img"
+                      onClick={onImageClick(value.id)}
+                      src={value.image}
+                    ></img>{" "}
+                    <p className="Test-result-name" title={value.name}>
+                      {value.name}
+                    </p>
+                  </div>
+                  <div className="Test-result-graph">graph</div>
+                </div>
+              ))}
 
-            <p>
-              {tastes[seletedTypeIdx].taste[0]}
-              {taste1}
-            </p>
-            <p>
-              {tastes[seletedTypeIdx].taste[1]}
-              {taste2}
-            </p>
-            <p>
-              {tastes[seletedTypeIdx].taste[2]}
-              {taste3}
-            </p>
-            <p>
-              {tastes[seletedTypeIdx].taste[3]}
-              {taste4}
-            </p>
-            <p>
-              {tastes[seletedTypeIdx].taste[4]}
-              {taste5}
-            </p>
-            <button className="next-button8 next-button" onClick={onNextClick}>
+            <button
+              className="next-button8 return-button"
+              onClick={onNextClick}
+            >
               처음으로
             </button>
           </div>
