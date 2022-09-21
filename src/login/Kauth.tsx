0d78fc50210a8d45d30341f2740c1010c0e2963d
mingmeng030 <djwls_0716@naver.com>
2022-09-21 17:40:18 +0900

    update Kauth url

--- src/login/Kauth.tsx
+++ src/login/Kauth.tsx
@@ -1,4 +1,4 @@
 const CLIENT_ID = "a17cc515ea9ec5c1ef5046107cbbcca2";
-const REDIRECT_URI = "http://localhost:3000/login";
+const REDIRECT_URI = "http://localhost:3000/auth/login";
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/orize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
